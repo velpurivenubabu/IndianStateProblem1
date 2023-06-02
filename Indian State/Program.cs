4d42bf9a228c2d2c49da935b3acf2e0101f9214c
venubabu <velpurivenubabu123@gmail.com>
2023-06-02 22:06:01 +0530

    [Venubabu] Done With Welcome message

---
+++ Indian State/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Indian_State
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Welcome to indian State Problems1!");
+        }
+    }
+}
